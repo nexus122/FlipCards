cdd98fd9e425fdd93f3d4c49eb5fa4928d657f70
Juan Pablo <juan-pablo122@hotmail.es>
2023-04-17 22:46:01 +0200

    Descargar Hummer.js directamente

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let cardsList = [];
 let cardCounter = 0;
 let maxCards = 0;
 
+let elemento = document.getElementById("mi-elemento");
+let hammertime = new Hammer(elemento);
+
 function createInteractions(text) {
   let card = document.querySelector(".card");
   let flip = document.querySelectorAll(".flipButton");
