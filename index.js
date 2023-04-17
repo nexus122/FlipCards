75e0eaa5555868fafd376cee34c0e63fb6888313
Juan Pablo <juan-pablo122@hotmail.es>
2023-04-17 23:57:17 +0200

    Pasamos todo a clases de js

--- index.js
+++ index.js
@@ -1,106 +1,124 @@
-let cardsList = [];
-let cardCounter = 0;
-let maxCards = 0;
+class Flashcard {
+  constructor() {
+    this.cardsList = [];
+    this.cardCounter = 0;
+    this.maxCards = 0;
+  }
 
-function createInteractions(text) {
-  let card = document.querySelector(".card");
-  let flip = document.querySelectorAll(".flipButton");
-  flip.forEach((flipButton) => {
-    flipButton.addEventListener("click", function () {
-      card.classList.toggle("flipped");
+  createInteractions(text) {
+    let card = document.querySelector(".card");
+    let flip = document.querySelectorAll(".flipButton");
+
+    flip.forEach((flipButton) => {
+      flipButton.addEventListener("click", () => {
+        card.classList.toggle("flipped");
+      });
     });
-  });
 
-  let sayButtons = document.querySelectorAll(".say");
-  sayButtons.forEach((sayButton) => {
-    sayButton.addEventListener("click", () => {
-      sayThePhrase(text);
+    let sayButtons = document.querySelectorAll(".say");
+
+    sayButtons.forEach((sayButton) => {
+      sayButton.addEventListener("click", () => {
+        this.sayThePhrase(text);
+      });
     });
-  });
 
-  let elemento = document.querySelector(".card");
-  let hammertime = new Hammer(elemento);
+    let elemento = document.querySelector(".card");
+    let hammertime = new Hammer(elemento);
 
-  hammertime.on("swipeleft", function (event) {
-    if (cardCounter == maxCards) return;
-    cardCounter++;
-    drawCard(cardsList[cardCounter]);
-  });
+    hammertime.on("swipeleft", (event) => {
+      if (this.cardCounter === this.maxCards) return;
+      this.cardCounter++;
+      this.drawCard(this.cardsList[this.cardCounter]);
+    });
 
-  hammertime.on("swiperight", function (event) {
-    if (cardCounter == 0) return;
-    cardCounter--;
-    drawCard(cardsList[cardCounter]);
-  });
-}
+    hammertime.on("swiperight", (event) => {
+      if (this.cardCounter === 0) return;
+      this.cardCounter--;
+      this.drawCard(this.cardsList[this.cardCounter]);
+    });
+  }
 
-function createCard(question, response) {
-  let card = `
+  createCard(question, response, example) {
+    let card = `
       <article class="card">
-      <div class="sideA">
+        <div class="sideA">
           <h1 class="say">${question}</h1>
           <button class="flipButton">Flip Card</button>
-      </div>
-      <div class="sideB">
-      <h1 class="say">${question}</h1>           
+        </div>
+        <div class="sideB">
+          <h1 class="say">${question}</h1>           
           <p>${response}</p>
+          <p>${example}</p>
           <button class="flipButton">Flip Card</button>
-      </div>
-  </article>
-  `;
-  cardsList.push(card);
-}
+        </div>
+      </article>
+    `;
 
-// Función que permite dibujar una unica carta
-function drawCard(card) {
-  document.querySelector(".deck").innerHTML = card;
-  let text = card.split('<h1 class="say">')[1].split("</h1>")[0];
-  createInteractions(text);
-}
+    this.cardsList.push(card);
+  }
 
-async function fetchData(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
+  async fetchData(url) {
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
     }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("There was a problem with the fetch operation:", error);
+  }
+
+  async init() {
+    let cards = await this.fetchData("./data.json");
+
+    cards.forEach((data) => {
+      this.createCard(data.question, data.response, data.example);
+    });
+
+    this.maxCards = cards.length - 1;
+    this.drawCard(this.cardsList[this.cardCounter]);
+  }
+
+  drawCard(card) {
+    document.querySelector(".deck").innerHTML = card;
+
+    let text = card.split('<h1 class="say">')[1].split("</h1>")[0];
+    this.createInteractions(text);
+  }
+
+  getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  sayThePhrase(text) {
+    const synth = window.speechSynthesis;
+    const utterThis = new SpeechSynthesisUtterance(text);
+    utterThis.lang = "it-IT";
+    synth.speak(utterThis);
+  }
+
+  handleBackButtonClick() {
+    if (this.cardCounter === 0) return;
+
+    this.cardCounter--;
+    this.drawCard(this.cardsList[this.cardCounter]);
+  }
+
+  handleNextButtonClick() {
+    if (this.cardCounter === this.maxCards) return;
+
+    this.cardCounter++;
+    this.drawCard(this.cardsList[this.cardCounter]);
   }
 }
 
-async function init() {
-  let cards = await fetchData("./data.json");
-  cards.forEach((data) => {
-    createCard(data.question, data.response);
-  });
-  maxCards = cards.length - 1;
-  drawCard(cardsList[cardCounter]);
-}
-
-function getRandomArbitrary(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-function sayThePhrase(text) {
-  const synth = window.speechSynthesis;
-  const utterThis = new SpeechSynthesisUtterance(text);
-  utterThis.lang = "it-IT";
-  synth.speak(utterThis);
-}
+const flashcard = new Flashcard();
 
 document.querySelector(".backButton").addEventListener("click", () => {
-  if (cardCounter == 0) return;
-  cardCounter--;
-  drawCard(cardsList[cardCounter]);
+  flashcard.handleBackButtonClick();
 });
-
-document.querySelector(".nextButton").addEventListener("click", () => {
-  if (cardCounter == maxCards) return;
-  cardCounter++;
-  drawCard(cardsList[cardCounter]);
-});
-
-init();
